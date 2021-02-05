eff0059626619c8fb986720b004d06abf04d3d94
Lim SeJin <43897724+LimSeJin9577@users.noreply.github.com>
2021-02-05 15:37:59 +0900

    Update run.py

--- Speech_to_Text/run.py
+++ Speech_to_Text/run.py
@@ -1,12 +1,10 @@
 from goog import transcribe_gcs
 
 # File Upload (connect with backend)
-PATH_mp4 = "C:\\Users\\cofls\\Desktop\\2021\\음성인식\\mp4\\"
-PATH_mp3 = "C:\\Users\\cofls\\Desktop\\2021\\음성인식\\mp3\\"
-speech_file = PATH_mp4 + "The_Expert__Progress_Meeting_(Short_Comedy_Sketch).mp4"
+PATH_mp4 = "/path/"
+speech_file = PATH_mp4 + "/.mp4"
+#speech_file = PATH_mp4 + "meeting_text.mp4"
 
-
-# speech_file = "/"
 
 # Runs transcription for each file inside the specified directory
 def run(mp4_file):
