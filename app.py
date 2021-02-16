6dc472d33be41041e5d64f1cbe6f1a46c6dbe62a
Lim SeJin <43897724+LimSeJin9577@users.noreply.github.com>
2021-02-16 17:42:59 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import warnings
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from ner import ner_visualize
 from werkzeug.utils import secure_filename
 from summarizer import Summarizer
@@ -19,8 +19,9 @@ def start_page():
 def html_page():
     if request.method == 'POST':
         f = request.files['file']
-        global FILENAME
+        global FILENAME, TEXTFILENAME
         FILENAME = f.filename
+        TEXTFILENAME = FILENAME.replace(".mp4", ".txt")
 
         # If file is not .mp4
         if ".mp4" not in FILENAME:
@@ -30,38 +31,56 @@ def html_page():
         speech_file = os.path.join('static/upload_files', secure_filename(FILENAME))
         googleAPI.transcribe_gcs(speech_file)
 
-    return render_template('HTMLPage1.html')
+    return render_template('HTMLPage1.html', file_name=FILENAME)
+
 
 '''완료----------------------------------------------------------------------------------------------------'''
 
+
 @app.route("/OriginPage")
 def origin():
-    text = open('./static/text_files/{}'.format(FILENAME.replace("mp4","txt")), mode='rt', encoding='utf-8')
+    text = open('./static/text_files/{}'.format(TEXTFILENAME), mode='rt', encoding='utf-8')
     text = text.read()
 
     return render_template('OriginPage.html', data=text)
 
 
+@app.route('/originpage_download', methods=["GET", "POST"])
+def origin_down():
+    origin_file = "static/text_files/{}".format(TEXTFILENAME)
+    return send_file(origin_file, attachment_filename=os.path.basename(origin_file),
+                     as_attachment=True)
+
+
 @app.route("/SummaryPage")
 def summary():
     # 텍스트 변수에 받아오기
-    text = open('./static/text_files/{}'.format(FILENAME.replace("mp4","txt")), mode='rt', encoding='utf-8')
+    text = open('./static/text_files/{}'.format(TEXTFILENAME), mode='rt', encoding='utf-8')
     text = text.read()
     model = Summarizer('distilbert-base-uncased')
     resp = model(text)
 
+    # make summary txt
+    with open('static/text_files/{}'.format(TEXTFILENAME.replace('.txt', '_summary.txt')), 'w') as txt_file:
+        txt_file.write(resp)
+
     return render_template('SummaryPage.html', data=resp)
 
+
+@app.route('/summarypage_download', methods=["GET", "POST"])
+def summary_down():
+    summary_file = "static/text_files/{}".format(TEXTFILENAME.replace('.txt', '_summary.txt'))
+    return send_file(summary_file, attachment_filename=os.path.basename(summary_file),
+                     as_attachment=True)
 
 
 @app.route("/NERPage")
 def ner():
     # 텍스트 변수에 받아오기
-    text = open('./static/text_files/{}'.format(FILENAME.replace("mp4", "txt")), mode='rt', encoding='utf-8')
+    text = open('./static/text_files/{}'.format(TEXTFILENAME), mode='rt', encoding='utf-8')
     ner_visualize(text)
-    return render_template('NERPage.html')
-
-
+    text.close()
+    return render_template('SpacyPage.html')
 
 
 if __name__ == "__main__":
--- ner.py
+++ ner.py
@@ -2,15 +2,24 @@ import os
 import spacy
 from spacy import displacy
 import warnings
+
 warnings.filterwarnings("ignore")
 
 
 def ner_visualize(text):
     nlp = spacy.load("en_core_web_sm")
+    text = text.read()
     doc = nlp(text)
     html = displacy.render(doc, style="ent", page=False)
 
-    with open('templates/SpacyPage.html', 'a') as html_file:
+    # DOWNLOAD
+    # # make spacy.txt
+    # with open('static/text_files/{}'.format(FILENAME.replace('.txt', '_spacy.txt')), 'w') as txt_file:
+    #     txt_file.write(html)
+
+    # make SpacyPage.html
+    with open('templates/SpacyPage.html', 'w') as html_file:
+        html_file.write('''{% extends "NERPage.html" %}{% block content %}''')
         html_file.write(html)
         html_file.write("{% endblock %}")
     return
