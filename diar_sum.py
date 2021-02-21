7766138fda57adb4eb20bc37ce18f819cc5044af
hiseoung <74298527+hiseoung@users.noreply.github.com>
2021-02-21 20:34:31 +0900

    Update diar_sum.py

--- diar_sum.py
+++ diar_sum.py
@@ -34,7 +34,7 @@ def speaker(x):
 def bertsum(filename, out_max_length, speaker_talk, speaker_num, total):
     from summarizer import Summarizer
 
-    with open("./static/text_files/{}".format(filename.replace('.txt', '_dr_sum_file.txt')), 'w') as f :
+    with open("./static/text_files/{}".format(filename.replace('.txt', '_dr_sum_file.txt')), 'w') as f:
         f.write(" More than " + str(speaker_num) + " people participated... <br>\n")
         for i in range(speaker_num):
             text = ". ".join(speaker_talk[i])
@@ -45,4 +45,50 @@ def bertsum(filename, out_max_length, speaker_talk, speaker_num, total):
         out_text = ". ".join(total)
         total_output = model(out_text, max_length=out_max_length)
         f.write("\nsummary : " + total_output)
+    return
+
+
+def bart(filename, x, total, speaker_num):
+    s = "More than " + str(speaker_num) + " people participated..<br>"
+    s1 = s + "<br>"
+    total = total
+    x = x.split("\n")
+    bart2(filename, 3, x, s1)
+    # bart2(filename, 4, total, "\ntotal")
+
+
+def bart2(filename, number, y, str):
+
+    from fastai.learner import load_learner
+    import torch
+    import pickle
+    import datasets
+    import re
+    import pathlib
+    temp = pathlib.PosixPath
+    pathlib.PosixPath = pathlib.WindowsPath
+
+    inf_learn = load_learner(fname='./data/bart_pretrained_SAMsum.pkl')
+    with open("./static/text_files/{}".format(filename.replace('.txt', '_dr_sum_file.txt')), 'w') as f:
+        f.write(str + "\n")
+        num = 0
+        s = ''
+        llist = []
+        for i in range(len(y)):
+            s = s + y[i]
+            if num == number:
+                llist.append(s)
+                num = 0
+                s = ''
+            num += 1
+        ll = []
+        for i in llist:
+            tt = inf_learn.blurr_generate(i)
+            a = tt.pop()
+            a = a.strip()
+            # a1 = re.sub(r'\([^)]*\)', '', a)
+            ll.append(a)
+            print(a)
+        for i in ll:
+            f.write(i + "\n")
     return
